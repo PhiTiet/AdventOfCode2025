bed21533b8341868116f07d3e482d3cf5a1c460f
Phi Tiet <anhphi099@gmail.com>
2025-12-18 10:47:38 +0100

    solves part1

--- src/main/kotlin/solution/day9/Day9ProblemSolver.kt
+++ src/main/kotlin/solution/day9/Day9ProblemSolver.kt
@@ -11,10 +11,25 @@ class Day9ProblemSolver : AbstractProblemSolver<Long>() {
 		.map{ RedTile(x =it[0], y =it[1])}
 
 	override fun partOne(): Long {
-		return 0
+		val rectangleSizes = mutableSetOf<Long>()
+		for (x in tiles) {
+			for (y in tiles) {
+				if (x == y ) {
+					continue
+				}
+				rectangleSizes.add(calculateRectangleArea(x, y))
+			}
+		}
+		return rectangleSizes.max()
 	}
 
 	override fun partTwo(): Long {
 		return 0
 	}
+	private fun calculateRectangleArea(tile1: RedTile, tile2: RedTile): Long {
+		val length = kotlin.math.abs(tile1.x - tile2.x + 1)
+		val width = kotlin.math.abs(tile1.y - tile2.y + 1)
+		return length * width
+	}
+
 }
